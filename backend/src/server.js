332f1d9c9fbe974ff193b0942967f2d5adc0b3d8
Matheus Teixeira Lampugnani <matheus.lampugnani@videplast.com.br>
2026-09-17 16:09:43 -0300

    Ajuste de caminho padrao Server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -259,10 +259,10 @@ app.get('/api/portaria/historico', async (req, res) => {
     return res.status(200).json(data);
 });
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
 });
 
 app.listen(10000, () => {
