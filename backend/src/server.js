ccc1e9e81d5ebc60a1fc4e14a295cceef84e1149
Matheus Teixeira Lampugnani <matheus.lampugnani@videplast.com.br>
2026-09-17 16:36:43 -0300

    Ajuste de caminho do frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -261,7 +261,7 @@ app.get('/api/portaria/historico', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
 });
 
